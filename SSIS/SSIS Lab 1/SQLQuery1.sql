f86505ff5b33769478adfadbd2d53f7000a858ac
Ashok R <ashokce80@gmail.com>
2019-06-13 18:43:18 -0500

    SSIS lab 1

--- SSIS/SSIS Lab 1/SQLQuery1.sql
+++ SSIS/SSIS Lab 1/SQLQuery1.sql
@@ -6,4 +6,24 @@ EMPID int,	EmpName varchar(50), JobTitle varchar(50),	Salary money)
 Select		*
 From		tblEmployee
 
+Create Database LearningSSIS2
 
+USE	LearningSSIS2
+
+Create Table	tblEmp (
+EMPID int,	EmpName varchar(50), JobTitle varchar(50),	Salary money)
+
+Select		*
+From		tblEmp
+
+Create Database Prospects
+
+Create table tbl_stg_Prospects 
+(	Name varchar(250),
+	Title varchar(250)
+	,Company varchar(250)
+	,Location varchar(250)
+)
+
+Select	*
+From	tbl_stg_Prospects 
